9e404ffe9601981a49fff1bb29cf7bc4f2f19a6d
willaparecido <wilson_aparecido2015@outlook.com>
2021-02-19 13:15:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(type(two_digit_number))
+
+#get the first and second digits using subscripting then convert string to int.
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+result = int(first_digit) + int(second_digit)
+print(result)
+
+
+
